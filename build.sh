6568830ea9f0fffc55fcd759608648ebbb006e7d
Dan Faulknor <danielfaulknor@users.noreply.github.com>
2022-05-23 18:38:01 +1200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+wget -qO - https://packages.confluent.io/deb/7.1/archive.key | apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/7.1 stable main"
+add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
+apt update
+apt install librdkafka-dev
+
 PROJDIR=`cd $(dirname $0); pwd -P`
 cd $PROJDIR
 echo `pwd`
